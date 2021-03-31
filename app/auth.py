3b72f02ed9d58126f484d510584bc12f852af0c8
Dennis Lau <dennislau@Denniss-MBP.hitronhub.home>
2021-03-31 15:14:18 -0700

    added fix

--- app/auth.py
+++ app/auth.py
@@ -8,66 +8,8 @@ from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from database import get_db
 from models.User import User
-from models.Dispatcher import Dispatcher
-from models.Group import Group
 from functools import wraps
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-
-def has_group_write_perms(func, *args, **kwargs):
-    """Decorator to Checks to see if user has group read permissions 
-    """
-    @wraps(func)
-    def inner(*args, **kwargs):
-        group = kwargs['session'].query(Group).get(kwargs['group_id'])
-        if group is None:
-            raise HTTPException(status.HTTP_400_BAD_REQUEST, "Group not found")
-        # if user is admin and in same organization or if user is in group or user is dispatcher assigned to group
-        if kwargs['current_user'].is_admin() and group.organization_id == kwargs['current_user'].organization_id:
-            return func(*args, **kwargs)
-        else:
-            raise HTTPException(status.HTTP_403_FORBIDDEN,
-                                "You dont have permissions to make this call")
-    return inner
-
-
-def has_group_read_perms(func, *args, **kwargs):
-    """Decorator to Checks to see if user has group read permissions 
-    """
-    @wraps(func)
-    def inner(*args, **kwargs):
-        group = kwargs['session'].query(Group).get(kwargs['group_id'])
-        if group is None:
-            raise HTTPException(status.HTTP_400_BAD_REQUEST, "Group not found")
-        # if user is admin and in same organization or if user is in group or user is dispatcher assigned to group
-        if (kwargs['current_user'].is_admin() and group.organization_id == kwargs['current_user'].organization_id) or group in kwargs['current_user'].groups or group in Dispatcher().get_by_user_id(kwargs['current_user'].id, kwargs['session']).groups:
-            return func(*args, **kwargs)
-        else:
-            raise HTTPException(status.HTTP_403_FORBIDDEN,
-                                "You dont have permissions to make this call")
-    return inner
-
-
-def has_dispatcher_read_update_perms(func):
-    """Decorator to check to see if current user has ability to read or update targeted dispatcher
-    Args:
-        current_user (User): current user in session
-        session (Session): current db session
-        dispatcher_id (int) id of dispatcher
-    """
-    @wraps(func)
-    def inner(*args, **kwargs):
-        dispatcher = kwargs['session'].query(
-            Dispatcher).get(kwargs['dispatcher_id'])
-        if dispatcher is None:
-            raise HTTPException(status.HTTP_400_BAD_REQUEST,
-                                "Dispatcher not found")
-        #  allow query if user in admin org, or if user id matches dispatcher user_id
-        if (kwargs['current_user'].is_admin() and dispatcher.user.organization_id == kwargs['current_user'].organization_id) or dispatcher.user_id == kwargs['current_user'].id:
-            return func(*args, **kwargs)
-        else:
-            raise HTTPException(status.HTTP_403_FORBIDDEN)
-    return inner
 
 
 def has_user_read_update_perms(func):
