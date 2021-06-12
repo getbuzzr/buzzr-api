98c19a3a5fc66169bf4e3d253b86d04223e25606
Dennis Lau <dennislau@Denniss-MacBook-Pro.local>
2021-06-12 11:58:15 -0700

    added log

--- app/scheduler.py
+++ app/scheduler.py
@@ -9,22 +9,25 @@ from utils import serialize, send_push_sns, generate_apple_order_push_payload
 
 
 def remove_abandoned_orders():
+    print("START ABANDON ORDER")
     with session_scope() as session:
         # get all orders that are checking out date_created > 5 mins ago
         ten_mins_since = datetime.datetime.utcnow() - datetime.timedelta(minutes=10)
         thirty_mins_since = datetime.datetime.utcnow() - datetime.timedelta(minutes=30)
         orders_ten_mins_over = session.query(Order).filter(and_(Order.status == OrderStatusEnum.checking_out,
                                                                 Order.date_created < ten_mins_since)).all()
+        print(orders_ten_mins_over)
         for order_ten_min_over in orders_ten_mins_over:
             # user has not been push notified, send push
             if not order_ten_min_over.is_user_abandoned_cart_notified:
                 user = order_ten_min_over.user
-                if user.apn_token:
-                    send_push_sns(user.apn_token, "ios",
-                                  generate_apple_order_push_payload("Looks like you are still checking out some items. Click here to finish your checkout!", OrderStatusEnum.paid))
-                if user.fcm_token:
-                    send_push_sns(user.fcm_token, "android",
-                                  "Looks like you are still checking out some items. Click here to finish your checkout!")
+                # TODO WRITE GOOD PUSH COPY
+                # if user.apn_token:
+                #     send_push_sns(user.apn_token, "ios",
+                #                   generate_apple_order_push_payload("Looks like you are still checking out", "", OrderStatusEnum.paid))
+                # if user.fcm_token:
+                #     send_push_sns(user.fcm_token, "android",
+                #                   "Looks like you are still checking out some items. Click here to finish your checkout!")
                 order_ten_min_over.is_user_abandoned_cart_notified = True
                 session.add(order_ten_min_over)
                 continue
