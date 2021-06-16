9ed65f79519241f5d4fa10f3c2931c298d6c3ec4
Dennis Lau <dennislau@Denniss-MacBook-Pro.local>
2021-06-16 11:13:01 -0700

    added fix

--- app/routers/tags.py
+++ app/routers/tags.py
@@ -5,8 +5,9 @@ from sqlalchemy.orm import Session
 # Models
 from models.Department import Department
 from models.User import User
-from models.Product import Product, ProductStatusEnum
+from models.Product import Product, ProductStatusEnum, product_tags
 from models.ProductTag import ProductTag
+from sqlalchemy import and_
 # schemas
 from schemas.ProductSchema import ProductSchemaOut
 from schemas.ProductTagSchema import ProductTagOut
@@ -35,10 +36,13 @@ def get_featured_tags():
 
 
 @router.get('/{tag_id}/products', response_model=List[ProductSchemaOut])
-def get_tag_items(department_id: int):
+def get_tag_items(tag_id: int):
     # serialize pictures/tags as they are one to many
-
     with session_scope() as session:
-        tag_products = session.query(Product).filter_by(
-            department_id=department_id, status=ProductStatusEnum.active).all()
-        return [serialize(x) for x in department_products]
+        product_tag = session.query(ProductTag).get(tag_id)
+        if product_tag is None:
+            raise HTTPException(status.HTTP_404_NOT_FOUND)
+        with session_scope() as session:
+            tag_products = session.query(Product).join(product_tags).filter(and_(
+                product_tags.c.tag_id == tag_id, Product.status == ProductStatusEnum.active)).all()[:10]
+            return [serialize(x) for x in tag_products]
